aee99f81805c285a4df2ca0b8c651af10ec89243
rsshigueoka <rafaelshigueoka@gmail.com>
2019-04-09 21:44:29 -0300

    +classe Categoria e EmpresaCliente
    implementação básica das classes Categoria e EmpresaCliente

---
+++ app/src/main/java/com/example/senai/xtudoandroid/xtudo/EmpresaCliente.java
@@ -0,0 +1,51 @@
+package com.example.senai.xtudoandroid.xtudo;
+
+public class EmpresaCliente
+{
+    private Pedido pedido;
+
+    public boolean realizarCadastro()
+    {
+        return true;
+    }
+
+    public Produto consultar(String nome)
+    {
+        return null;
+    }
+
+    public boolean comprar(Produto p)
+    {
+        return true;
+    }
+
+    public boolean terminar()
+    {
+        return true;
+    }
+
+    public boolean pagar()
+    {
+        return true;
+    }
+
+    public Pedido obterStatusPedido()
+    {
+        return null;
+    }
+
+    public boolean enviarMensagem(String msg)
+    {
+        return true;
+    }
+
+    public boolean aceitarEntrega(boolean aceite)
+    {
+        return true;
+    }
+
+    public boolean cancelarPedido()
+    {
+        return true;
+    }
+}
