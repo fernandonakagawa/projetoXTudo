dd8e456d23f64ad3d9dd3bac6dadcac9d3914bf2
Thiago <thiagolukakakki@gmail.com>
2019-04-09 21:56:17 -0300

    .
    .

--- app/src/main/java/com/example/senai/xtudoandroid/xtudo/AvaliacaoGeral.java
+++ app/src/main/java/com/example/senai/xtudoandroid/xtudo/AvaliacaoGeral.java
@@ -6,4 +6,14 @@ public class AvaliacaoGeral {
     private float notaMediaPreco;
     private float notaMediaTempo;
     public boolean adicionarAvaliacao;
+
+public AvaliacaoGeral(int aAvaliacao,float notaMediaQualidade,float notaMediaPreco,
+                      float notaMediaTempo,boolean adicionarAvaliacao){
+    this.aAvaliacao = aAvaliacao;
+    this.notaMediaQualidade = notaMediaQualidade;
+    this.notaMediaPreco = notaMediaPreco;
+    this.notaMediaTempo = notaMediaTempo;
+    this.adicionarAvaliacao = adicionarAvaliacao;
 }
+}
+
